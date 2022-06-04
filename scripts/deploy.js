b4966f983f696744d220a28fbc37e2aa2d66293e
Muhammad Hammad Shahid <hammad.shahid.pk@gmail.com>
2022-06-05 01:11:05 +0500

    deployment script added

---
+++ scripts/deploy.js
@@ -0,0 +1,20 @@
+const {ethers} = require("hardhat")
+async function main()
+{
+    //getting the nft contract GameItem from NFTee.sol to deploy
+    const nftContract = await ethers.getContractFactory("GameItem");
+
+    //deployment of contract
+    const deployednftContract = await nftContract.deploy();
+
+    //printing the address of deployed nft contract
+    console.log("NFT contract address is: ",deployednftContract);
+}
+
+//calling the main funtion
+main().
+then(()=>process.exit(0))
+.catch((error)=>{
+    console.log(error)
+    process.exit(1);
+});
